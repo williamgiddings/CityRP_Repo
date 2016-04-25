787101075f1d6536c8bb14ffb9f42129e094247a
williamgiddings <wgidz99@gmail.com>
2016-04-25 10:58:49 +0100

    bleach.lua
    -Added drinkable bleach that drains hunger.

---
+++ gamemodes/arista/gamemode/items/food/bleach.lua
@@ -0,0 +1,8 @@
+ITEM.name					= "Bleach"
+ITEM.cost					= 5
+ITEM.model				= "models/props_junk/garbage_plasticbottle001a.mdl"
+ITEM.store				= true
+ITEM.plural				= "Bottles of Bleach"
+ITEM.hunger				= -100
+ITEM.description	= "Household cleaning product that certainly is not for drinking. Keep out of reach of children."
+ITEM.base					= "food"
