c206e990b372bab3566527757942253d29ca4c5a
williamgiddings <wgidz99@gmail.com>
2016-04-25 14:30:46 +0100

    fixed bleach.lua
    Yea it kills now, be careful...

--- gamemodes/arista/gamemode/items/food/bleach.lua
+++ gamemodes/arista/gamemode/items/food/bleach.lua
@@ -1,8 +1,17 @@
+
 ITEM.name					= "Bleach"
+ITEM.size					= 1
 ITEM.cost					= 5
 ITEM.model				= "models/props_junk/garbage_plasticbottle001a.mdl"
+ITEM.batch				= 5
 ITEM.store				= true
 ITEM.plural				= "Bottles of Bleach"
-ITEM.hunger				= -100
 ITEM.description	= "Household cleaning product that certainly is not for drinking. Keep out of reach of children."
-ITEM.base					= "food"
+ITEM.equippable		= true
+ITEM.equipword		= "drink bleach"
+ITEM.base					= "item"
+
+function ITEM:onUse(player)
+	-- todo: lang
+	player:SetHealth(math.Clamp(player:Health() - 75, 0, 100))
+end
